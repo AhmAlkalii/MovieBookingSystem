638986d303114153dc191fc14af2ebc48a06ec8d
Ahmed Alkali Mohammed <hmkalkali@gmail.com>
2024-07-27 16:35:58 +0200

    User Completed

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,26 @@
 const express =  require('express');  
 require('dotenv').config();
 const mongoose = require('mongoose'); 
+const bodyParser = require('body-parser')
+const UserRoutes = require('./routes/user')
+
 
 const app = express(); 
-app.use(express.json()); 
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
 
 mongoose.connect(process.env.Mongo_URI).then(()=>{
     app.listen(process.env.PORT,()=>{
-        console.log('running')
+        console.log(`Server is Listening on`, process.env.PORT, 'And Connect to MongoDB' )
     })
 }   
 ).catch((error)=>{
     console.log(error);
 })
+
+app.use('/api/user', UserRoutes)
+
---
+++ backend/routes/user.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const {getUsers,getGuests,singleUser,UserSignup,Userlogin,Guestlogin} = require('../controller/userController')
+
+const router = express.Router()
+
+//get Users
+router.get('/users', getUsers)
+//get Guests
+router.get('/guests', getGuests)
+//single User
+router.get('/:id',singleUser)
+//Signup
+router.post('/signup', UserSignup)
+//FrequentUser Login
+router.post('/login',Userlogin)
+//Guest Login
+router.post('/guestlogin',Guestlogin)
+
+
+module.exports = router
